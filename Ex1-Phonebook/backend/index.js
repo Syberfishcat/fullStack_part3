5270b8a0233f593da6601559c06092d25b0c708b
sybercat <sybercat@163.com>
2026-04-23 22:41:16 +0300

    initail backend and frontend files

---
+++ Ex1-Phonebook/backend/index.js
@@ -0,0 +1,117 @@
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+
+morgan.token('body', (req, res) => {
+    return JSON.stringify(req.body)
+})
+
+let persons = [
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+const idGeneration = () => {
+    if(persons.length === 0) return "1"
+    const id = Math.max(...persons.map(person => Number(person.id))) + 1
+    return String(id)
+}
+
+app.use(express.json())
+app.use(
+    morgan(function (tokens, req, res) {
+        return [
+            tokens.method(req, res),
+            tokens.url(req, res),
+            tokens.status(req, res),
+            tokens.res(req, res, 'content-length'), '-',
+            tokens['response-time'](req, res), 'ms',
+            tokens.body(req, res)
+        ].join(' ')
+    })
+)
+
+app.get('/api/persons', (request,response) => {
+    response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+    const time = new Date()
+    const content = `
+        <p>
+            Phonebook has info for ${persons.length} people
+        </p>
+        <p>
+            ${time.toString()}
+        </p>
+    `
+    response.send(content)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const person = persons.find(person => person.id === request.params.id)
+
+    if(person) {
+        response.json(person)
+    }else{
+        response.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    persons = persons.filter(person => person.id !== id)
+
+    response.status(204).end()
+})
+
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    if(!body.name){
+        return response.status(400).json({
+            error: 'name missing'
+        })
+    }
+    if(!body.number){
+        return response.status(400).json({
+            error: 'number missing'
+        })
+    }
+    if(persons.find(person => person.name === body.name)){
+        return response.status(400).json({
+            error: 'name must be unique'
+        })
+    }
+    const newPerson = {
+        name: body.name,
+        number: body.number,
+        id: idGeneration()
+    }
+
+    persons = persons.concat(newPerson)
+    response.json(newPerson)
+})
+
+const PORT = 3001
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
