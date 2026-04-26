771a3457672dc4945ec14526939f95a8f5534595
sybercat <sybercat@163.com>
2026-04-27 01:41:59 +0300

    add some functions that fetch/delete/modify data from database

--- Ex1-Phonebook/backend/index.js
+++ Ex1-Phonebook/backend/index.js
@@ -5,6 +5,17 @@ const Person = require('./models/person')
 
 const app = express()
 
+const errorHandler = (error, request, response, next) => {
+    console.log(error)
+
+    if(error.name === 'CastError') {
+        response.status(400).send({ error: 'malformatted id'})
+    }
+
+    next(error)
+}
+
+
 morgan.token('body', (req, res) => {
     return JSON.stringify(req.body)
 })
@@ -32,12 +43,6 @@ let persons = [
     }
 ]
 
-// const idGeneration = () => {
-//     if(persons.length === 0) return "1"
-//     const id = Math.max(...persons.map(person => Number(person.id))) + 1
-//     return String(id)
-// }
-
 app.use(express.json(), express.static('dist'))
 app.use(
     morgan(function (tokens, req, res) {
@@ -58,30 +63,41 @@ app.get('/api/persons', (request,response) => {
     })
 })
 
-app.get('/info', (request, response) => {
+app.get('/info', (request, response, next) => {
     const time = new Date()
-    const content = `
-        <p>
-            Phonebook has info for ${persons.length} people
-        </p>
-        <p>
-            ${time.toString()}
-        </p>
-    `
-    response.send(content)
+    Person.find({})
+        .then(persons => {
+            const content = `
+                <p>
+                    Phonebook has info for ${persons.length} people
+                </p>
+                <p>
+                    ${time.toString()}
+                </p>
+            `
+            response.send(content)
+        })
+        .catch(error => next(error))
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    Person.findById(request.params.id).then(person => {
-        response.json(person)
-    })
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+        .then(person => {
+            if(!person){
+                return response.status(404).end()
+            }
+
+            response.json(person)
+        })
+        .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    persons = persons.filter(person => person.id !== id)
-
-    response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndDelete(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
 
@@ -99,21 +115,34 @@ app.post('/api/persons', (request, response) => {
         })
     }
 
-    // if(persons.find(person => person.name === body.name)){
-    //     return response.status(400).json({
-    //         error: 'name must be unique'
-    //     })
-    // }
-
     const person = new Person({
         name: body.name,
         number: body.number
     })
 
-    person.save().then(savedNote => {
-        response.json(savedNote)
+    person.save().then(savedPerson => {
+        response.json(savedPerson)
     })
 })
+
+app.put('/api/persons/:id', (request, response, next) => {
+    const {name, number} = request.body
+
+    Person.findById(request.params.id)
+        .then(person => {
+            console.log(person)
+            if(!person){
+                return response.status(404).end()
+            }
+            person.name = name
+            person.number = number
+
+            return person.save().then(updatedPerson => response.json(updatedPerson))
+        })
+        .catch(error => next(error))
+})
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
