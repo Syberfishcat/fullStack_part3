704355f8b5f37f37da854996a489e25d670a7b5a
sybercat <sybercat@163.com>
2026-04-27 13:05:04 +0300

    fix validator of number

--- Ex1-Phonebook/backend/models/person.js
+++ Ex1-Phonebook/backend/models/person.js
@@ -22,7 +22,7 @@ const personSchema = new mongoose.Schema({
     number: {
         type: String,
         validate: {
-            validator: v => /\d{2,3}-\d+/.test(v),
+            validator: v => /^\d{2,3}-\d+$/.test(v),
             message: props => `${props.value} is not a valid phone number!`
         }
     }
