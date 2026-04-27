70b3abd40c22067336e5aafde9774950c76a9905
sybercat <sybercat@163.com>
2026-04-27 12:49:58 +0300

    uibuild

--- Ex1-Phonebook/frontend/src/App.jsx
+++ Ex1-Phonebook/frontend/src/App.jsx
@@ -59,6 +59,12 @@ const App = () => {
             setNotification(null)
           }, 5000)
         })
+        .catch(error => {
+            setErrorMessage(error.response.data.error)
+            setTimeout(() => {
+              setErrorMessage(null)
+            }, 5000)
+        })
     }
     setNewName('')
     setNewNumber('')
